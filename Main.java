046c872c23ba1d2501c18ee1e35f50aca0c4bb7e
Noelsky-code <hansorly@naver.com>
2021-02-07 20:09:31 +0900

    에라토스테레스체 오랜만 ..

--- Main.java
+++ Main.java
@@ -3,6 +3,20 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
+        String[] seoul = {"Jane","Kim"};
+
+        StringBuilder sb= new StringBuilder();
+        int cnt=0;
+        for(String i : seoul){
             
+            if(i.equals("Kim")){
+                break;                
+            }
+            cnt++;
+        }
+        sb.append("김서방은 ");
+        sb.append(cnt);
+        sb.append("에 있다");
+        System.out.println(sb.toString());
     }
 }
