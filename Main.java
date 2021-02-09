ccef278bbd9655741a46059e2fd112ee89445b64
Noelsky-code <hansorly@naver.com>
2021-02-10 01:41:01 +0900

    구현문제.

--- Main.java
+++ Main.java
@@ -1,47 +1,32 @@
-
 import java.util.*;
-//시간복잡도 생각안하고 구현 1단계 문제보다 더쉬운듯? 
-public class Main {
-
-
-    public static void main(String[] args){
-        int[] priorities={2,1,3,2};
-        int location=2;
-        LinkedList<Text> list = new LinkedList<Text>();
-        for(int i=0;i<priorities.length;i++){
-            list.addLast(new Text(i,priorities[i]));
-        }
-        int answer=0;
-        while(true){
-            int max= get_max(list);//
-            Text temp= list.removeFirst();
-            if(temp.priority==max){
-                answer++;
-                if(temp.idx==location){
-                    break;
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        
+        
+        for(int i=0;i<skill_trees.length;i++){
+            int len =skill_trees[i].length();
+            boolean tag=true;
+            LinkedList<String> list = new LinkedList<String>();
+            LinkedList<String> skills=new LinkedList<String>();
+            for(int j=0;j<len;j++){
+                list.addLast(skill_trees[i].substring(j,j+1));
+            }
+            for(int j=0;j<skill.length();j++){
+                skills.addLast(skill.substring(j,j+1));
+            }
+            for(int j=0;j<len;j++){
+                String temp = list.removeFirst();
+                if(skills.contains(temp)){//스킬에 포함되있다면
+                    if(!skills.removeFirst().equals(temp)){
+                        tag=false;
+                        break;
+                    }
                 }
             }
-            else{
-                list.addLast(temp);
-            }
+            if(tag)answer++;
+            
         }
-        System.out.println(answer);
-    }
-    static int get_max(LinkedList<Text> list){
-        int max=0;
-        for(int i=0;i<list.size();i++){
-            max=Math.max(list.get(i).priority,max);
-        }
-        return max;
-        
-    }
-
-}
-class Text{
-    int idx;
-    int priority;
-    public Text(int idx,int priority){
-        this.idx=idx;
-        this.priority=priority;
+        return answer;
     }
 }
