fa6768ffa15991c624dd8d9d2ab055f572255a6c
Noelsky-code <hansorly@naver.com>
2021-02-16 01:16:26 +0900

    임시

--- Main.java
+++ Main.java
@@ -1,51 +1,21 @@
 import java.util.*;
 
 public class Main {
+    //전체탐색시 시간 초과 -> 그리디로 
     public static void main(String[] args) {
-        String s = "abcabcabcabcdededededede";
-        //String s= "abcabcdede";
-        int n=1;
-        int answer = s.length();
-        int len= s.length();
-        while(n<=len/2){
-            
-            int ret=0;
-
-            String[] temp = new String[(len/n)+1];
-            int cnt=0;
-            for(int i=0;cnt<(len/n);i+=n){
-            
-                temp[cnt++]=s.substring(i, i+n);//cnt에 배당완료 
-                 //System.out.println(temp[cnt-1]);
-            }
-            cnt=0;//동일한거 카운트하기 
-            Stack<String> stack = new Stack<String>();
-            stack.push(temp[0]);
-            for(int i=1;i<(len/n);i++){
-                if(temp[i].equals(stack.peek())){
-                   cnt++;     
-                }
-                else{
-                    ret+=n;
-                    if(cnt>0){
-                        ret++;
-                        cnt=0;
-                    }
-                    stack.pop();
-                    stack.push(temp[i]);
-                }
-            }
-            if(cnt>0){
-                ret++;
-            }
-            ret+=n;
-            answer=Math.min(answer,ret);
-           // System.out.println();
-            
-            n++;
+        String number = "4177252841";
+        int k=4;
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0;i<number.length();i++){
+            map.put(number.charAt(i)-'0', 0);
         }
-        System.out.println(answer);
-
+        for(int i=0;i<number.length();i++){
+            map.put(number.charAt(i)-'0',map.get(i)+1);
+        }
+        //1.숫자 제일 큰거 구하기 
+        //2-1. 왼쪽에 제거할거 갯수(a)가 k보다 작을시 제거 -> k-a
+        //2-2 . 클경우 더 왼쪽으로 이동  
+        //3-1. 제일 큰거 오른쪽에서 또 다시 .. 
     }
        
 }
