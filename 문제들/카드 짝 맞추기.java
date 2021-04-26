480775848ed651f1b57070524f58d6190e26d88b
Noelsky-code <hansorly@naver.com>
2021-04-27 02:07:59 +0900

    극한의 구현을 경험한 것 같음 .. 최소 5시간은 걸린거같음 .. bfs로 풀었다면 더 빨리 풀었을것 같은데 .. 너무 dfs만 고집하지 말자

--- 문제들/카드 짝 맞추기.java
+++ 문제들/카드 짝 맞추기.java
@@ -1,40 +1,81 @@
+package src;
+
 import java.util.*;
 
 public class Main{
     static int board_global[][];
-    static int check=0;
-    static int before_r;
-    static int before_c;
     static HashSet<String> per;
     static HashSet<Integer> set;
+    static int count;
+    
 
     static Point[] points;
     public static void main(String[] args){
-        
+        count=0;
         int[][] board={{1,0,0,3},{2,0,0,0},{0,0,0,2},{3,0,1,0}};
         int r=1;int c=0;
         
         board_global = board;
-        before_r=0;
-        before_c=0;
         per = new HashSet<String>();
         set = new HashSet<Integer>();    
         boolean[] visit = new boolean[7];
         make_set();//존재하는 카드 종류를 set에 넣음
-        points = new Point[set.size()*2];
+        points = new Point[set.size()*2+1];
 
         points[0]=new Point(r,c,0);
         make_points();
         make_per(visit,"");//카드 순열을 만들어 per에 넣어줌
-        for(String s:per){//1,2   3,4  5,6 
-            
+       
+        int answer= 987654321;
+         for(String s:per){//1,2   3,4  5,6 
+            answer=Math.min(answer,check(s,-1,-1,1));
         }
-       
-       
-    
-    }
-    public static void check(){
         
+        System.out.println(answer+set.size()*2);
+        System.out.println(count);
+    }
+    public static int check(String s,int now,int cnt,int visit){//cnt에 방문한 점 index. 
+        int ret=987654321;
+        if(now==s.length()-1){
+            if(cnt%2==1){//1,3,5
+                ret=find_dist(points[cnt],points[cnt+1],make_max(points[cnt], points[cnt+1]),0);
+            }
+            else{
+                ret=find_dist(points[cnt],points[cnt-1],make_max(points[cnt], points[cnt-1]),0);
+            }
+            //System.out.println(ret);
+            return ret;
+        }
+        int next = (s.charAt(now+1)-'0')*2;//만약 1이라면 1~2 2라면 3 
+        
+        if(now==-1){//0일때
+            Point p = points[0];
+            ret=Math.min(ret,check(s,now+1,next-1,visit|(1<<(next-1)))+find_dist(p,points[next-1],make_max(p, points[next-1]),0));
+            ret=Math.min(ret,check(s,now+1,next,visit|(1<<next))+find_dist(p,points[next],make_max(p, points[next]),0));
+            return ret;
+        }//return 되니까 고려 x 
+        int temp = (s.charAt(now)-'0')*2;// temp는 now *2 
+        if((visit&(1<<(temp)))==(1<<temp)&&((visit&(1<<(temp-1)))==(1<<(temp-1)))){//두점 모두 방문한 상태면 다음으로 넘어감. 
+            board_global[points[temp-1].x][points[temp-1].y] = 0;
+            board_global[points[temp].x][points[temp].y]=0;
+            ret=Math.min(ret,check(s,now+1,next-1,visit|(1<<(next-1)))+find_dist(points[cnt],points[next-1],make_max(points[cnt], points[next-1]),0));
+            ret=Math.min(ret,check(s,now+1,next,visit|(1<<(next)))+find_dist(points[cnt],points[next],make_max(points[cnt], points[next]),0));
+            board_global[points[temp-1].x][points[temp-1].y] = points[temp].n;
+            board_global[points[temp].x][points[temp].y]=points[temp].n;
+        }
+        else{//둘중 한점만 방문
+
+            if(cnt%2==1){//1,3,5
+                ret=check(s,now,cnt+1,visit|(1<<(cnt+1)))+find_dist(points[cnt],points[cnt+1],make_max(points[cnt], points[cnt+1]),0);
+            }
+            else{
+                ret=check(s,now,cnt-1,visit|(1<<(cnt-1)))+find_dist(points[cnt],points[cnt-1],make_max(points[cnt], points[cnt-1]),0);
+            }
+
+        }
+        //System.out.println(ret);
+        return ret;
+
     }
 
     /*public static void make_arr(){
@@ -45,25 +86,34 @@ public class Main{
             }
         }
     }*/
-    public static int find_dist(Point from,Point to,int max,int cnt){//목표 지점 찾는 과정 
-        int ret = Math.abs(from.x-to.x)+ Math.abs(from.y-to.y);
-        if(from.x==to.x&&from.y==to.y){
-            return 0;
+    public static int make_max(Point from,Point to){
+        return Math.abs(from.x-to.x)+ Math.abs(from.y-to.y);
+    }
+    public static int find_dist(Point from,Point to,int max,int cnt){//거리계산 . 흠 ...  
+        //여기 때문에 시간초과남. 개선해야하는데 흠 ... 
+        //힌트 얻었다 .. bfs를 쓰면 되겠구나 ..하아 .. 왜 생각을 못했을까 ... 다른건 다 생각했는데 ㅠㅠ 
+        int[][] move={{1,0},{-1,0},{0,1},{0,-1}};
+        Queue<Point> queue = new LinkedList<Point>();
+        queue.add(new Point(from.x,from.y,0));
+        while(!(queue.peek().x==to.x&&queue.peek().y==to.y)){
+            Point p = queue.poll();
+            for(int i=0;i<4;i++){
+                int[] next = ctrl(i,p.x,p.y);
+                queue.add(new Point(next[0],next[1],p.n+1));
+            }
+            for(int i=0;i<4;i++){
+                int row = p.x+move[i][0];
+                int col = p.y+move[i][1];
+                if(row>=0&&row<board_global.length&&col>=0&&col<board_global[0].length){
+                    queue.add(new Point(row,col,p.n+1));
+                }
+            }         
         }
-        
-        if(cnt>=max){
-            return 987654321;
-        }
-        
-        for(int i=0;i<4;i++){
-            int[] next = ctrl(i,from.x,from.y);
-            ret=Math.min(ret,1+find_dist(new Point(next[0],next[1],-1),to,max,cnt+1));
-        }
-        return ret;
+        return queue.peek().n;
         
     }
     public static void make_points(){
-        int cnt=0;
+        int cnt=1;
         for(int i:set){
             for(int j=0;j<4;j++){
                 for(int k=0;k<4;k++){
@@ -114,30 +164,34 @@ public class Main{
     }
     public static int[] ctrl_up(int r,int c){
         
-        while(r!=0&&board_global[r][c]!=0){
-            r=r-1;  
+        while(r!=0){
+            r=r-1;
+            if(board_global[r][c]!=0)break;  
         }
         int out[] ={r,c};
         return out;
         
     }
     public static int[] ctrl_down(int r,int c){
-        while(r!=board_global.length-1&&board_global[r][c]!=0){
-            r=r+1;  
+        while(r!=board_global.length-1){
+            r=r+1;
+            if(board_global[r][c]!=0)break;    
         }
         int out[] ={r,c};
         return out;
     }
     public static int[] ctrl_left(int r,int c){
-        while(c!=0&&board_global[r][c]!=0){
-            c=c-1;  
+        while(c!=0){
+            c=c-1;
+            if(board_global[r][c]!=0)break;    
         }
         int out[] ={r,c};
         return out;
     }
     public static int[] ctrl_right(int r,int c){
-        while(c!=board_global[0].length-1&&board_global[r][c]!=0){
+        while(c!=board_global[0].length-1){
             c=c+1;  
+            if(board_global[r][c]!=0)break;  
         }
         int out[] ={r,c};
         return out;
