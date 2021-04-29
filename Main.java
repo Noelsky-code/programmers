def27eb9407d741c17082a6c01e6c198573c0d1b
Hong Hansol <hansorly@ajou.ac.kr>
2021-04-30 03:48:41 +0900

    can_move_ down 이용해서 up,left,right 구현 해야함, 그다음 move 구현하고 rotate도 구현하면됨

--- Main.java
+++ Main.java
@@ -1,109 +1,63 @@
+
 import java.util.*;
 
-public class Main {
-    static String a="";
-    public static void main(String[] args) {
-        String[] info={"java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"};
-        String[] query={"java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"};
-        String[][] inf=new String[info.length][5];
-        String[][] que=new String[query.length][5];
-        
+public class Main{
 
-        for(int i=0;i<info.length;i++){
-            inf[i] = info[i].split(" ");
-        }
-        for(int i=0;i<query.length;i++){
-            query[i]=query[i].replaceAll("and", "");
-            query[i]=query[i].replaceAll("  ", " ");
-            que[i]=query[i].split(" ");
-        }
-        ArrayList[] arr=new ArrayList[24];
+    public static void main(String[] args){
+        int[][] board= {{0, 0, 0, 1, 1},{0, 0, 0, 1, 0},{0, 1, 0, 1, 1},{1, 1, 0, 0, 1},{0, 0, 0, 0, 0}};
+        Robot temp = new Robot();
+        System.out.println(temp.can_move_down(board));
+        //만약 move할곳이 없다면 return 해줌 
+    }
+    //왼쪽 위쪽 오른쪽 이동 함수 구현, 갈수 있는지 없는지 구현 . 
+    static void aaaa(){
+        //can_move_down() ??? -> true => move 
+    }
+    static void rotate(){
+        //can_move_down() ?? -> true  -> 아래로 rotate 가능 
+    }
+}
 
-       // int size[]=new int[36];
-        
-       // ArrayList<Integer> list =new ArrayList<Integer>();
-        
-        HashMap<String,Integer> map =new HashMap<String,Integer>();
-        int cnt=0;
-        for(int i=0;i<inf.length;i++){//20만번 가능 
-            StringBuilder sb= new StringBuilder();
-            for(int j=0;j<4;j++){
-                sb.append(inf[i][j]);
+class Robot{
+    int[] left;
+    int[] right;
+    boolean dir;//dir이 true면 가로방향 ,dir이 false면 세로로  
+    public Robot(){
+        left= new int[2];
+        right= new int[2];
+        left[0]=0;
+        left[1]=0;
+        right[0]=0;
+        right[1]=1;
+        dir=true;
+    }
+    public void maintain(){//왼쪽 오른쪽 기준을 잡기위함 . 항상 왼쪽이 오른쪽보다 작음
+        if(this.left[0]>this.right[0]||this.left[1]<this.right[1]){
+            int[] temp= this.left;
+            this.left[0] = this.right[0];
+            this.left[1]= this.right[1];
+            this.right = this.left;
+        }
+    }
+    public boolean can_move_down(int[][] board){
+        if(dir){//누운 상태라면
+            int row= this.left[0];
+            if(row==board.length-1){
+                return false;
             }
-            if(!map.containsKey(sb.toString())){
-                arr[cnt]=new ArrayList<Integer>(); 
-                map.put(sb.toString(),cnt++);
+            if(board[row+1][this.left[1]]==0&&board[row+1][this.right[1]]==0){
+                return true;
             }
-            int n=map.get(sb.toString());
-            arr[n].add(Integer.parseInt(inf[i][4]));
+            return false;
         }
-        for(int i=0;i<cnt;i++){
-            Collections.sort(arr[i]);
+        int row = this.right[0];
+        if(row==board.length-1){
+            return false;
         }
-        //-는 어떻게 처리하지???
-        int[] answer= new int[que.length];
-        for(int i=0;i<que.length;i++){//n logn ==> 이분탐색 
-            make_s(que[i],0,"");
-            String temp[]=a.split(" ");
-            a="";
-            for(int j=0;j<temp.length;j++){
-                String t= temp[j];
-                if(!map.containsKey(t)){
-                    continue;
-                }
-                int idx= map.get(temp[j]);
-                /*for(int k=0;k<size[idx];k++){
-                    System.out.println(arr[idx][k]);
-                }*/
-                if(arr[idx].size()==0)continue;
-                answer[i]+=arr[idx].size()-find(arr[idx],Integer.parseInt(que[i][4]),0,arr[idx].size()-1);//이분탐색 log n 
-
-            }
+        if(board[row+1][this.right[1]]==0){
+            return true;
         }
-        for(int i:answer){
-            System.out.println(i);
-        }
-       
+        return false; 
+    }
     
-    }
-    static void make_s(String s[],int cnt,String temp){
-        if(cnt>3){
-            if(a.length()==0){
-                a=temp+" ";
-            }
-            else{
-                a=a+temp+" ";
-            }
-            return;
-        }
-        if(cnt==0&&s[0].equals("-")){
-            make_s(s,cnt+1,temp+"java");
-            make_s(s,cnt+1,temp+"cpp");
-            make_s(s,cnt+1,temp+"python");
-        }
-        else if(cnt==1&&s[1].equals("-")){
-            make_s(s,cnt+1,temp+"backend");
-            make_s(s,cnt+1,temp+"frontend");
-        }
-        else if(cnt==2&&s[2].equals("-")){
-            make_s(s,cnt+1,temp+"junior");
-            make_s(s,cnt+1,temp+"senior");
-        }
-        else if(cnt==3&&s[3].equals("-")){
-            make_s(s,cnt+1,temp+"chicken");
-            make_s(s,cnt+1,temp+"pizza");
-        }
-        else{
-            make_s(s,cnt+1,temp+s[cnt]);
-        }
-    }
-    static int find(ArrayList<Integer> arr,int num,int left,int right){
-        int mid=(left+right)/2;
-        if(left>right)return left;
-        if(left>arr.size())return right;
-        if(arr.get(mid)>=num){
-            return find(arr,num,left,mid-1); 
-        }
-        return find(arr,num,mid+1,right);
-    }
 }
